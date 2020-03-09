f0c58df2aa48cffcaaafb257cc14cfb3e8d195df
unknown <--global>
2020-03-09 19:39:54 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class TodoList extends Component{
 
-export default App;
+constructor(){
+  super();
+  this.state={
+    todos:[],
+    currentToDo: "",
+  }
+};
+
+handleChange = event => {
+  console.log(event.target.value);
+};
+addTask = event => {
+  this.setState({
+    currentToDo: event.target.value
+  });
+  console.log("The add Task method was exeuctued")
+};
+ onSubmit = (event) => {
+   event.preventDefault()
+   this.setState({
+     currentToDo: '',
+     todos: [...this.state.todos,this.state.currentToDo]
+
+   });
+ }
+
+  render(){
+    return(
+      <div className="container">
+      <form onSubmit={this.addTask}>
+      <label htmlFor="taskName">Task Name:</label>
+      <input onChange={this.handleChange} name="taskName"type="text"placeholder="Add todo here!" />
+        {/*Add submit to form*/}
+      <button type="submit"> Add task </button>
+      </form>
+      </div>
+    );
+  };
+};
+
+export default TodoList;
